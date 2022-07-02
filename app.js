4fff2be046bbbd5805c826ca6f9d19a108585809
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-07-02 12:53:54 -0400

    inital commit for tankmates backend

---
+++ app.js
@@ -0,0 +1,13 @@
+//import code for express
+const express = require('express');
+
+//initialize app
+const app = express();
+
+//route
+app.get('/', (request, response) => {
+    response.send("Hello World")
+})
+
+//export app
+module.exports = app;
