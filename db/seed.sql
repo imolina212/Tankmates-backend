a8c89834afc6b998d362a4113d02cfe845482470
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-09-02 10:22:23 -0400

    add more seed data to species table

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,14 @@
+\c tankmates_app;
+
 INSERT INTO species (id, species_name, scientific_name, origin, pic)
 VALUES
-(1, 'Crystal Red Shrimp', 'Caridina Shrimp', 'China', 'https://aquadiction.world/img/profile/crystal-red-shrimp2.jpg'),
-(2, 'Red Cherry Shrimp', 'Caridina Shrimp', 'China', 'https://www.serendipitywave.com/wp-content/uploads/2020/08/Neocaridina-davidi-Red-Sakura.jpg'),
+(1, 'Crystal Red Shrimp', 'Caridina cf. cantonensis', 'China', 'https://aquadiction.world/img/profile/crystal-red-shrimp2.jpg'),
+(2, 'Red Cherry Shrimp', 'Neocaridina Heteropoda', 'China', 'https://www.serendipitywave.com/wp-content/uploads/2020/08/Neocaridina-davidi-Red-Sakura.jpg'),
 (3, 'Bristlenose Plecostomus', 'Ancistrus cirrhosus', 'South America', 'https://www.keepingtropicalfish.co.uk/wp-content/uploads/2012/01/bushy-nose-plecostomus-500p.jpg'),
-(4, 'Flower Horn Cichlid', 'Amphilophus hybrid', 'South America', 'https://i.ytimg.com/vi/pbHt34o2hUw/maxresdefault.jpg');
+(4, 'Flower Horn Cichlid', 'Amphilophus hybrid', 'South America', 'https://i.ytimg.com/vi/pbHt34o2hUw/maxresdefault.jpg'),
+(5, 'Frontosa Cichlid', 'Cyphotilapia Frontosa', 'Lake Tanganyika', 'https://cdn.shopify.com/s/files/1/1960/6211/products/frontosa_2b_grande.jpg?v=1529613292'),
+(6, 'Peacock Cichlid', 'Aulonocara', 'Lake Malawi', 'https://www.aquaticdesign.co.uk/wp-content/uploads/2013/03/Red-cichlid-375.jpg'),
+(7, 'Goldfish', 'Carassius auratus', 'East Asia', 'https://vetstudentstories.merckmanuals.com/wp-content/uploads/sites/2/2017/08/iStock-514649164.jpg'),
+(8, 'Betta', 'Betta splendens', 'Southeast Asia', 'https://www.aquastore.in/image/cache/catalog/aquastore.in/1.Storeimages/1.Aquarium/1.Aquastore/2.betta/fancy/Fancy-Marble-Rosetail-Halfmoon-Betta-Fish-600x600.jpg'),
+(9, 'Arowana', 'Scleropages formosus', 'Southeast Asia', 'http://aquaticaa.weebly.com/uploads/1/7/6/2/17629499/2287097_orig.jpg'),
+(10, 'Piranha', 'Pygocentrus nattereri', 'South America', 'https://explorersweb.com/wp-content/uploads/2020/03/shutterstock_1597150267-e1585239060526.jpg');
