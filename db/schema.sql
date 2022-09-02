99b627b045827eaf63f45b928db512418fb30213
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-09-01 21:34:32 -0400

    add schema file

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+-- Drop if exists
+
+DROP DATABASE IF EXISTS tankmates_app;
+
+CREATE DATABASE tankmates_app;
+
+\c tankmates_app;
+
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    species_name TEXT,
+    scientific_name TEXT,
+    origin TEXT,
+    pic TEXT
+);
