006123690d024a716f9e32be7321e0fd45d055ea
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-10-20 18:46:25 -0400

    updated species tables, add origin table

--- db/schema.sql
+++ db/schema.sql
@@ -7,36 +7,24 @@ CREATE DATABASE tankmates_app;
 \c tankmates_app;
 
 CREATE TABLE species (
-    species_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     species_name TEXT,
     scientific_name TEXT,
-    origin TEXT,
-    pic TEXT
-);
-
-DROP TABLE IF EXISTS plants;
-
-CREATE TABLE plants (
-    plant_id SERIAL PRIMARY KEY,
-    plant_name TEXT,
-    plant_scientific_name TEXT,
-    plant_type TEXT,
-    placement TEXT,
-    light_requirements TEXT,
-    origin TEXT,
-    co2_requirements TEXT,
-    skill_level TEXT,
+    origin_id INT REFERENCES origin(id),
     pic TEXT,
+    species_type TEXT,
+    min_temp INT,
+    max_temp INT,
+    min_ph DECIMAL,
+    max_ph DECIMAL,
+    temperament TEXT,
+    max_length DECIMAL
 );
 
-DROP TABLE IF EXISTS products;
 
-CREATE TABLE products (
-    product_id SERIAL PRIMARY KEY,
-    product_name TEXT,
-    product_brand TEXT,
-    product_image TEXT,
-    product_description TEXT,
-    product_rating TEXT,
-    product_price TEXT,
+DROP TABLE IF EXISTS origin;
+
+CREATE TABLE origin (
+    id SERIAL PRIMARY KEY,
+    origin_name TEXT
 );
