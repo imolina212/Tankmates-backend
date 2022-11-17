a92969189154bd19dea191b6a402dc1a62140298
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-11-16 22:21:19 -0500

    add singleSpeciesByName query

--- queries/species.js
+++ queries/species.js
@@ -1,25 +1,45 @@
 const db = require("../db/index.js");
 
 const getSpeciesList = async () => {
-    try {
-        const speciesList = await db.any("SELECT * FROM species");
-        return speciesList;
-
-    } catch (err) {
-        return err;
-    }
-}
+	try {
+		const speciesList = await db.any("SELECT * FROM species");
+		return speciesList;
+	} catch (err) {
+		return err;
+	}
+};
 
 const getSingleSpecies = async (id) => {
-    try {
-        const singleSpecies = await db.one("SELECT * FROM species WHERE id=$1", id);
-        return singleSpecies;
-    } catch (err) {
-        throw err;
-    }
-}
+	try {
+		const singleSpecies = await db.one(
+			"SELECT * FROM species WHERE id=$1",
+			id
+		);
+		return singleSpecies;
+	} catch (err) {
+		throw err;
+	}
+};
+
+const getSingleSpeciesByName = async (speciesName, filters = "") => {
+	const searchTerm = `%${speciesName}%`;
+	try {
+		const singleSpecies = await db.any(
+			`SELECT * FROM species
+            WHERE LOWER(species_name) LIKE LOWER($1) 
+			AND id NOT IN ($2:csv)
+			LIMIT 5;`,
+			[searchTerm, filters.split(",")]
+		);
+		return singleSpecies;
+	} catch (err) {
+		console.log("getSingleSpeciesByName", err);
+		throw err;
+	}
+};
 
 module.exports = {
-    getSpeciesList,
-    getSingleSpecies,
-}
+	getSpeciesList,
+	getSingleSpecies,
+	getSingleSpeciesByName,
+};
