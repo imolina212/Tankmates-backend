348239bf639124958334adb6974e037b23209c8a
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-12-12 19:21:02 -0500

    adaptable db connection for deployment

--- db/index.js
+++ db/index.js
@@ -1,23 +1,33 @@
-const pgp = require('pg-promise')();
+const pgp = require("pg-promise")();
 require("dotenv").config();
 
-// const databaseURL = process.env.DB_URL;
+// const DB_URL = process.env.DB_URL;
 
-const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } = process.env;
+const {
+	DATABASE_URL,
+	PG_HOST,
+	PG_PORT,
+	PG_DATABASE,
+	PG_USER,
+	PG_PASSWORD,
+	DB_URL,
+} = process.env;
 
-const cn = DATABASE_URL ? {
-    connectionString : DATABASE_URL,
-    max: 30,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-} : {
-    host: PG_HOST,
-    port: PG_PORT,
-    database: PG_DATABASE,
-    user: PG_USER,
-    password: PG_PASSWORD,
-};
+const cn = DB_URL
+	? {
+			connectionString: DB_URL,
+			max: 30,
+			ssl: {
+				rejectUnauthorized: false,
+			},
+	  }
+	: {
+			host: PG_HOST,
+			port: PG_PORT,
+			database: PG_DATABASE,
+			user: PG_USER,
+			password: PG_PASSWORD,
+	  };
 
 const db = pgp(cn);
 
